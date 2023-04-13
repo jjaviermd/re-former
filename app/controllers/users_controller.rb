8327f92e5143244462f8fe789fb8b15b44ac5253
jjavier <razornice@gmail.com>
2023-04-13 11:22:58 -0400

    error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "New user created!"
       redirect_to new_user_path
     else
+      flash.now[:error] = "It was not posible"
       render :new, status: :unprocessable_entity
     end
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
